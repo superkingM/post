1835315c2673ef0ae307f76543945406399d0a69
smallK <396656156@qq.com>
2020-01-09 17:34:32 +0800

    数据库提交

--- README.md
+++ README.md
@@ -1,78 +1,18 @@
-<p align="center"><img src="https://res.cloudinary.com/dtfbvvkyp/image/upload/v1566331377/laravel-logolockup-cmyk-red.svg" width="400"></p>
+# 介绍
+本项目是使用laravel6.2进行开发的一个简单的博客项目，非常的适合新手，基本学习完就能动手开发其他的项目，比如企业网站
 
-<p align="center">
-<a href="https://travis-ci.org/laravel/framework"><img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/d/total.svg" alt="Total Downloads"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/v/stable.svg" alt="Latest Stable Version"></a>
-<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/license.svg" alt="License"></a>
-</p>
+**涉及功能**
 
-## About Laravel
+- 登录功能
+- 基本CURD
+- 中间件
+- 验证码
+- 语言包
+- 表单验证
+- 上传图片
+- 模型关联关系
+- 辅助函数
 
-Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
+如果你是学习laravel的新手，比较大的项目难以看懂，那么这个项目非常的适合新手laravel入门，有什么问题可以加QQ群427336263进行咨询
 
-- [Simple, fast routing engine](https://laravel.com/docs/routing).
-- [Powerful dependency injection container](https://laravel.com/docs/container).
-- Multiple back-ends for [session](https://laravel.com/docs/session) and [cache](https://laravel.com/docs/cache) storage.
-- Expressive, intuitive [database ORM](https://laravel.com/docs/eloquent).
-- Database agnostic [schema migrations](https://laravel.com/docs/migrations).
-- [Robust background job processing](https://laravel.com/docs/queues).
-- [Real-time event broadcasting](https://laravel.com/docs/broadcasting).
-
-Laravel is accessible, powerful, and provides tools required for large, robust applications.
-
-## Learning Laravel
-
-Laravel has the most extensive and thorough [documentation](https://laravel.com/docs) and video tutorial library of all modern web application frameworks, making it a breeze to get started with the framework.
-
-If you don't feel like reading, [Laracasts](https://laracasts.com) can help. Laracasts contains over 1500 video tutorials on a range of topics including Laravel, modern PHP, unit testing, and JavaScript. Boost your skills by digging into our comprehensive video library.
-
-## Laravel Sponsors
-
-We would like to extend our thanks to the following sponsors for funding Laravel development. If you are interested in becoming a sponsor, please visit the Laravel [Patreon page](https://patreon.com/taylorotwell).
-
-- **[Vehikl](https://vehikl.com/)**
-- **[Tighten Co.](https://tighten.co)**
-- **[Kirschbaum Development Group](https://kirschbaumdevelopment.com)**
-- **[64 Robots](https://64robots.com)**
-- **[Cubet Techno Labs](https://cubettech.com)**
-- **[Cyber-Duck](https://cyber-duck.co.uk)**
-- **[British Software Development](https://www.britishsoftware.co)**
-- **[Webdock, Fast VPS Hosting](https://www.webdock.io/en)**
-- **[DevSquad](https://devsquad.com)**
-- [UserInsights](https://userinsights.com)
-- [Fragrantica](https://www.fragrantica.com)
-- [SOFTonSOFA](https://softonsofa.com/)
-- [User10](https://user10.com)
-- [Soumettre.fr](https://soumettre.fr/)
-- [CodeBrisk](https://codebrisk.com)
-- [1Forge](https://1forge.com)
-- [TECPRESSO](https://tecpresso.co.jp/)
-- [Runtime Converter](http://runtimeconverter.com/)
-- [WebL'Agence](https://weblagence.com/)
-- [Invoice Ninja](https://www.invoiceninja.com)
-- [iMi digital](https://www.imi-digital.de/)
-- [Earthlink](https://www.earthlink.ro/)
-- [Steadfast Collective](https://steadfastcollective.com/)
-- [We Are The Robots Inc.](https://watr.mx/)
-- [Understand.io](https://www.understand.io/)
-- [Abdel Elrafa](https://abdelelrafa.com)
-- [Hyper Host](https://hyper.host)
-- [Appoly](https://www.appoly.co.uk)
-- [OP.GG](https://op.gg)
-
-## Contributing
-
-Thank you for considering contributing to the Laravel framework! The contribution guide can be found in the [Laravel documentation](https://laravel.com/docs/contributions).
-
-## Code of Conduct
-
-In order to ensure that the Laravel community is welcoming to all, please review and abide by the [Code of Conduct](https://laravel.com/docs/contributions#code-of-conduct).
-
-## Security Vulnerabilities
-
-If you discover a security vulnerability within Laravel, please send an e-mail to Taylor Otwell via [taylor@laravel.com](mailto:taylor@laravel.com). All security vulnerabilities will be promptly addressed.
-
-## License
-
-The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+本项目各个功能讲解地址：[laravel6急速入门开发](https://www.kancloud.cn/kingsurper/noobolaravel)
--- post.sql
+++ post.sql
@@ -10,10 +10,98 @@ Target Server Type    : MYSQL
 Target Server Version : 50724
 File Encoding         : 65001
 
-Date: 2020-01-06 18:47:52
+Date: 2020-01-09 17:09:14
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for articles
+-- ----------------------------
+DROP TABLE IF EXISTS `articles`;
+CREATE TABLE `articles` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci,
+  `category_id` int(11) DEFAULT NULL,
+  `sort` int(11) DEFAULT NULL,
+  `view` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
+  `markdown_html_code` longtext COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of articles
+-- ----------------------------
+INSERT INTO `articles` VALUES ('1', null, '2020-01-09 02:33:46', '123', '21', '1', '0', '0', '1', '/storage/images/2oqCWbXI7DirDKIQfJAXgXdvagN5zaDyfHPdOiWl.jpeg', '222', '<p>21</p>');
+INSERT INTO `articles` VALUES ('4', '2020-01-08 07:00:48', '2020-01-08 07:00:48', '你好世界', null, '1', null, null, null, '/storage/images/2oqCWbXI7DirDKIQfJAXgXdvagN5zaDyfHPdOiWl.jpeg', null, null);
+INSERT INTO `articles` VALUES ('5', '2020-01-08 08:32:29', '2020-01-08 08:32:29', '测试的标题', '# 测试标题\r\n你好世界\r\n![](/uploads/20200108163211_855820.jpg)', '1', null, null, null, '/storage/images/cw2VGtM471WPc49Lw8dyJao3mwx9jErt2W6rO7lC.jpeg', '测试文章', '<h1 id=\"h1-u6D4Bu8BD5u6807u9898\"><a name=\"测试标题\" class=\"reference-link\"></a><span class=\"header-link octicon octicon-link\"></span>测试标题</h1><p>你好世界<br><img src=\"/uploads/20200108163211_855820.jpg\" alt=\"\">');
+INSERT INTO `articles` VALUES ('6', '2020-01-09 15:44:10', '2020-01-09 15:44:10', '打发大水', '大大范德萨发生的覆盖啥的给对方鬼地方个地方大萨达', '1', null, null, null, '/storage/images/GaAePW4XzGQN2gcy6GlvvxchxJIMTjmTGW6h6CUc.jpeg', '颠三倒四', '<p>大大范德萨发生的覆盖啥的给对方鬼地方个地方大萨达</p>');
+INSERT INTO `articles` VALUES ('7', '2020-01-09 15:44:34', '2020-01-09 15:44:34', '鬼地方个放大官方', '工位风格AW范围为二位AFE水电费所发生的广东省大事发生', '1', null, null, null, '/storage/images/UspEPrLhIxoqMA4g6URSfXULq2CCGc8mBmpaTZHs.jpeg', '打法胜多负少的', '<p>工位风格AW范围为二位AFE水电费所发生的广东省大事发生</p>');
+INSERT INTO `articles` VALUES ('8', '2020-01-09 15:44:57', '2020-01-09 15:44:57', '发生大幅度更让人', '发送到发发送到发送到', '2', null, null, null, '/storage/images/a3AUvKakljAXi6APorWD62vzS7wwv80T6CS5l4vl.jpeg', '发士大夫撒阿萨德发', '<p>发送到发发送到发送到</p>');
+INSERT INTO `articles` VALUES ('9', '2020-01-09 15:45:19', '2020-01-09 15:45:19', '大使馆我', '大东方4然他 人头啊', '3', null, null, null, '/storage/images/TX16NMPcZMlEVnCVZEEsHRXpdYVe9jxHsGjPCRl9.jpeg', '大沙发上', '<p>大东方4然他 人头啊</p>');
+INSERT INTO `articles` VALUES ('10', '2020-01-09 15:45:53', '2020-01-09 15:45:53', '发顺丰森', '服务分未发二天 广东人广东人果然跟', '2', null, null, null, '/storage/images/lMDdG9KjuLsT7hucvmPuAspDoQaK1nDTew2015SY.jpeg', 'fQW3供热费为', '<p>服务分未发二天 广东人广东人果然跟</p>');
+INSERT INTO `articles` VALUES ('11', '2020-01-09 15:47:43', '2020-01-09 15:49:54', '宜家抽屉柜砸死两岁男童，赔3.2亿！', '近日，宜家与美国加州一名被马尔姆（MALM）系列抽屉柜压死的两岁男童的家人达成了和解，同意赔偿4600万美元（约合人民币3.2亿）。2020年1月8日，南方都市报记者查询宜家中国官网发现，该系列抽屉柜仍在售。\r\n![](/uploads/20200109154723_326565.jpg)\r\n2017年5月，该名男童在家中被翻倒的宜家马尔姆系列三斗抽屉柜压住，窒息死亡。国家市场监管总局2016年7月发布的召回信息显示，宜家全球总共收到122份家具倾倒报告，有57起造成人员受伤，其中6起造成人员死亡的事故均发生在美国。\r\n\r\n　　受事件影响，2017年11月，美国消费品安全委员（CPSC）与宜家重新向消费者强调一年前发布后未引起足够关注的召回计划，涉及共计1730万件抽屉柜和梳妆柜。宜家被质疑未对产品风险作充分警示，未对消费者尽召回计划的告知义务。\r\n\r\n　　南都记者查询发现，2016年7月，经国家质检总局约谈后，宜家（中国）投资有限公司决定自即日在中国市场上召回1999年至2016年期间销售的马尔姆等系列抽屉柜。中国大陆地区受影响的产品（包括进口产品）数量共计166万件。\r\n\r\n　　缺陷产品管理中心的公告提醒称，召回的橱柜和抽屉柜不稳固，如果没有正确固定到墙上，可造成严重的倾翻和扣压危险，可能会造成儿童受伤或死亡。\r\n\r\n　　“夺命抽屉柜”事件持续发酵后，2017年11月30日，上海市质量技监局官方网站发出通告称，针对社会关注的宜家“马尔姆”橱柜系列产品召回情况，市质监局已约谈宜家（中国）投资有限公司，要求企业切实履行召回义务，立即组织开展缺陷调查分析。\r\n\r\n　　2020年1月8日，南都记者查询发现，宜家官网上马尔姆系列抽屉柜仍在售。宜家中国相关负责人对“召回但不停售”曾解释称，宜家提供的措施都能够保证产品在使用过程中的安全性。宜家官网公告称，该公司免费提供上墙连接件，并为用户提供免费上门安装支持；消费者也可将家具退回宜家商场，获全额退款。', '1', null, null, null, '/storage/images/1w4sbbFdIirBWZnqGNmIDpbQRQvyxgATS582JfGX.jpeg', '近日，宜家与美国加州一名被马尔姆（MALM）系列抽屉柜压死的两岁男童的家人达成了和解，同意赔偿4600万美元（约合人民币3.2亿）。2020年1月8日，南方都市报记者查询宜家中国官网发现，该系列抽屉柜仍在售。', '<p>近日，宜家与美国加州一名被马尔姆（MALM）系列抽屉柜压死的两岁男童的家人达成了和解，同意赔偿4600万美元（约合人民币3.2亿）。2020年1月8日，南方都市报记者查询宜家中国官网发现，该系列抽屉柜仍在售。<br><img src=\"/uploads/20200109154723_326565.jpg\" alt=\"\"><br>2017年5月，该名男童在家中被翻倒的宜家马尔姆系列三斗抽屉柜压住，窒息死亡。国家市场监管总局2016年7月发布的召回信息显示，宜家全球总共收到122份家具倾倒报告，有57起造成人员受伤，其中6起造成人员死亡的事故均发生在美国。\r\n<p>　　受事件影响，2017年11月，美国消费品安全委员（CPSC）与宜家重新向消费者强调一年前发布后未引起足够关注的召回计划，涉及共计1730万件抽屉柜和梳妆柜。宜家被质疑未对产品风险作充分警示，未对消费者尽召回计划的告知义务。</p>\r\n<p>　　南都记者查询发现，2016年7月，经国家质检总局约谈后，宜家（中国）投资有限公司决定自即日在中国市场上召回1999年至2016年期间销售的马尔姆等系列抽屉柜。中国大陆地区受影响的产品（包括进口产品）数量共计166万件。</p>\r\n<p>　　缺陷产品管理中心的公告提醒称，召回的橱柜和抽屉柜不稳固，如果没有正确固定到墙上，可造成严重的倾翻和扣压危险，可能会造成儿童受伤或死亡。</p>\r\n<p>　　“夺命抽屉柜”事件持续发酵后，2017年11月30日，上海市质量技监局官方网站发出通告称，针对社会关注的宜家“马尔姆”橱柜系列产品召回情况，市质监局已约谈宜家（中国）投资有限公司，要求企业切实履行召回义务，立即组织开展缺陷调查分析。</p>\r\n<p>　　2020年1月8日，南都记者查询发现，宜家官网上马尔姆系列抽屉柜仍在售。宜家中国相关负责人对“召回但不停售”曾解释称，宜家提供的措施都能够保证产品在使用过程中的安全性。宜家官网公告称，该公司免费提供上墙连接件，并为用户提供免费上门安装支持；消费者也可将家具退回宜家商场，获全额退款。</p>');
+
+-- ----------------------------
+-- Table structure for article_tags
+-- ----------------------------
+DROP TABLE IF EXISTS `article_tags`;
+CREATE TABLE `article_tags` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of article_tags
+-- ----------------------------
+INSERT INTO `article_tags` VALUES ('10', '4', '10');
+INSERT INTO `article_tags` VALUES ('11', '4', '11');
+INSERT INTO `article_tags` VALUES ('12', '4', '12');
+INSERT INTO `article_tags` VALUES ('13', '4', '13');
+INSERT INTO `article_tags` VALUES ('14', '5', '14');
+INSERT INTO `article_tags` VALUES ('15', '1', '15');
+INSERT INTO `article_tags` VALUES ('16', '1', '16');
+INSERT INTO `article_tags` VALUES ('17', '6', '17');
+INSERT INTO `article_tags` VALUES ('18', '6', '18');
+INSERT INTO `article_tags` VALUES ('19', '7', '19');
+INSERT INTO `article_tags` VALUES ('20', '7', '20');
+INSERT INTO `article_tags` VALUES ('21', '7', '21');
+INSERT INTO `article_tags` VALUES ('22', '8', '22');
+INSERT INTO `article_tags` VALUES ('23', '8', '23');
+INSERT INTO `article_tags` VALUES ('24', '9', '23');
+INSERT INTO `article_tags` VALUES ('25', '9', '24');
+INSERT INTO `article_tags` VALUES ('26', '10', '25');
+INSERT INTO `article_tags` VALUES ('27', '10', '26');
+INSERT INTO `article_tags` VALUES ('28', '10', '27');
+INSERT INTO `article_tags` VALUES ('29', '11', '28');
+INSERT INTO `article_tags` VALUES ('30', '11', '29');
+
+-- ----------------------------
+-- Table structure for categories
+-- ----------------------------
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of categories
+-- ----------------------------
+INSERT INTO `categories` VALUES ('1', '测试', '0000-00-00 00:00:00', null);
+INSERT INTO `categories` VALUES ('2', '测试3', '2020-01-07 08:51:26', '2020-01-07 09:05:52');
+INSERT INTO `categories` VALUES ('3', '阿士大夫撒范德萨发撒', '2020-01-09 02:45:46', '2020-01-09 02:45:46');
 
 -- ----------------------------
 -- Table structure for migrations
@@ -24,11 +112,51 @@ CREATE TABLE `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
 -- Records of migrations
 -- ----------------------------
+INSERT INTO `migrations` VALUES ('1', '2020_01_07_074613_create_categories_table', '1');
+INSERT INTO `migrations` VALUES ('2', '2020_01_07_094932_create_articles_table', '2');
+INSERT INTO `migrations` VALUES ('3', '2020_01_08_010740_create_tags_table', '3');
+INSERT INTO `migrations` VALUES ('4', '2020_01_08_011051_create_article_tags_table', '4');
+
+-- ----------------------------
+-- Table structure for tags
+-- ----------------------------
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE `tags` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Records of tags
+-- ----------------------------
+INSERT INTO `tags` VALUES ('10', '2020-01-08 07:00:48', '2020-01-08 07:00:48', '世界');
+INSERT INTO `tags` VALUES ('11', '2020-01-08 07:00:48', '2020-01-08 07:00:48', '你好');
+INSERT INTO `tags` VALUES ('12', '2020-01-08 07:00:48', '2020-01-08 07:00:48', '你好');
+INSERT INTO `tags` VALUES ('13', '2020-01-08 07:00:48', '2020-01-08 07:00:48', '世界');
+INSERT INTO `tags` VALUES ('14', '2020-01-08 08:32:29', '2020-01-08 08:32:29', '测试');
+INSERT INTO `tags` VALUES ('15', '2020-01-09 02:33:46', '2020-01-09 02:33:46', '哈哈');
+INSERT INTO `tags` VALUES ('16', '2020-01-09 02:33:46', '2020-01-09 02:33:46', '你好啊');
+INSERT INTO `tags` VALUES ('17', '2020-01-09 15:44:10', '2020-01-09 15:44:10', '颠三倒四');
+INSERT INTO `tags` VALUES ('18', '2020-01-09 15:44:10', '2020-01-09 15:44:10', '辅导辅导');
+INSERT INTO `tags` VALUES ('19', '2020-01-09 15:44:34', '2020-01-09 15:44:34', '热热');
+INSERT INTO `tags` VALUES ('20', '2020-01-09 15:44:34', '2020-01-09 15:44:34', '大萨达');
+INSERT INTO `tags` VALUES ('21', '2020-01-09 15:44:34', '2020-01-09 15:44:34', '范德萨发的');
+INSERT INTO `tags` VALUES ('22', '2020-01-09 15:44:57', '2020-01-09 15:44:57', '发生大幅度是');
+INSERT INTO `tags` VALUES ('23', '2020-01-09 15:44:57', '2020-01-09 15:44:57', '官方');
+INSERT INTO `tags` VALUES ('24', '2020-01-09 15:45:19', '2020-01-09 15:45:19', '给对方');
+INSERT INTO `tags` VALUES ('25', '2020-01-09 15:45:53', '2020-01-09 15:45:53', '发的');
+INSERT INTO `tags` VALUES ('26', '2020-01-09 15:45:53', '2020-01-09 15:45:53', '古典风格');
+INSERT INTO `tags` VALUES ('27', '2020-01-09 15:45:53', '2020-01-09 15:45:53', '地方');
+INSERT INTO `tags` VALUES ('28', '2020-01-09 15:47:43', '2020-01-09 15:47:43', '宜家');
+INSERT INTO `tags` VALUES ('29', '2020-01-09 15:47:43', '2020-01-09 15:47:43', '抽屉柜');
 
 -- ----------------------------
 -- Table structure for users
